0c38d9ca24147280825763ced9f661d14b21c936
Barbajhonny <Giovanniscano52@gmail.com>
2026-07-09 17:13:00 +0200

    updated contract removed import"forge-std" added interface cheatcodes halmos

--- regression/call_verifier/halmos/CallVerifier_call-failure_v1.t.sol
+++ regression/call_verifier/halmos/CallVerifier_call-failure_v1.t.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.25;
 
 import "target/CallVerifier_v1.sol";
-import "forge-std/Test.sol";
 
-contract CallVerifierTest is Test {
+interface IHalmosVM {
+    function load(address target, bytes32 slot) external view returns (bytes32);
+}
+
+contract CallVerifierTest {
     CallVerifier cv;
+
+    IHalmosVM constant vm = IHalmosVM(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function setUp() public {
         cv = new CallVerifier();
